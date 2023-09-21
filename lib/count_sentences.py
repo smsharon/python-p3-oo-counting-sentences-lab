68f41339d071bd52fb7ad95e86cf639e32df76c0
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-09-21 04:53:57 -0400

    completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
-
+import re
 class MyString:
-  pass
+  def __init__(self, value = ""):
+    self._value = ""
+    self.value = value
+  def get_value(self):
+    return self._value
+  def set_value(self, value):
+    if type(value) == str:
+      self._value = value 
+    else:
+      print("The value must be a string.")  
+  value = property(get_value, set_value)       
+
+    
+    
+  def is_sentence(self):
+        return True if  self._value.endswith('.') else False  
+  def is_question(self):
+            return True if  self._value.endswith('?') else False 
+  def is_exclamation(self):
+              return True if  self._value.endswith('!') else False
+  def count_sentences(self):
+    sentences = re.split(r'[.!?]', self._value)
+    sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+    return len(sentences)            
+  
+work = MyString()
+#print(work.value)
+print(work.count_sentences())
+#work.value = "please work"
+#print(work.value)
+#work.value = 89
+#print (work.value)
+#work.value = "This is a string! It has three sentences. Right?"
+#print(work.count_sentences())
